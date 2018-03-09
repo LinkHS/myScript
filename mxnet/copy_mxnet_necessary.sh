a95717959b487f37146114687a0191767567ad08
austin.dawei <dawei.yang@hobot.cc>
2018-03-09 11:06:48 +0800

    add script to copy necessary folders from comiled mxnet

---
+++ mxnet/copy_mxnet_necessary.sh
@@ -0,0 +1,19 @@
+set -x
+SOURCE_PATH=/home/austin/work/hobot-mxnet
+
+rm -rf bin
+rm -rf deps
+rm -rf include
+rm -rf lib
+rm -rf plugin
+rm -rf python
+rm -rf tools
+
+cp -r $SOURCE_PATH/bin      bin
+cp -r $SOURCE_PATH/deps     deps
+cp -r $SOURCE_PATH/include  include
+cp -r $SOURCE_PATH/lib      lib
+cp -r $SOURCE_PATH/plugin   plugin
+cp -r $SOURCE_PATH/python   python
+cp -r $SOURCE_PATH/tools    tools
+
